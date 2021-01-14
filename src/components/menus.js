2192ad4fd2075eaf2bc947ea3b105037dc516d3a
DevCode01 <36292698+DevCode01@users.noreply.github.com>
2021-01-14 18:40:46 +0100

    Update menus.js

--- src/components/menus.js
+++ src/components/menus.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../App.css';
-import { MemoryRouter, BrowserRouter, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Menus extends React.Component {
 
@@ -20,14 +20,12 @@ class Menus extends React.Component {
   render() {
     return (
       <div>
-        <MemoryRouter>
         <ul>
           <li><Link className="monMenu" to="/">Home</Link></li>
           <li><Link className="monMenu" to="/page2">Sport</Link></li>
           <li><Link className="monMenu" to="/page3">Économie</Link></li>
           <li><Link className="monMenu" to="/page4">Ajouter un article</Link></li>
         </ul>
-        </MemoryRouter>
       </div>
     );
   }
